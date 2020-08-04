864ff7b5f6c3094642d8c4de5fda7239f928b434
Jone Bulande <JoneBulande@gmail.com>
2020-08-04 10:26:44 +0200

    validar o formulário

--- js/script - Copia.js
+++ js/script - Copia.js
@@ -1,30 +1,30 @@
 const form = document.querySelector("form");
 const btnLogin = document.querySelector(".btn");
 
-btnLogin.addEventListener("click", event => {
+function remover() {
+	form.classList.remove("validate-error");
+}
+btnLogin.addEventListener("click", event =>{
 	event.preventDefault();
 	
 	const inputs = [...document.querySelectorAll("input")];
-	inputs.forEach(input => {
-		if (input.value == "") form.classList.add("validate-error");
+	inputs.forEach(input =>{
+		if (input.value == "") {
+			form.classList.add("validate-error");
+			setTimeout(remover, 500);
+		}else{
+			form.classList.remove("validate-error");
+			form.classList.add("form-hide");
+		}
 	});
-	const formErro = document.querySelector(".validate-error");
-	if (formErro) {
-		formErro.addEventListener("animationend", event => {
-			if (event.animationName == "nono") {
-				form.classList.remove("validate-error");
-			}
-		})
-	}else{
-		form.classList.add("form-hide");
-	}
-		
 })
+
 form.addEventListener("animationstart", event => {
 	if (event.animationName == "down") {
 		document.querySelector("body").style.overflow = "hidden";
 	}
 });
+
 form.addEventListener("animationend", event => {
 	if (event.animationName == "down") {
 		form.style.display = "none";
