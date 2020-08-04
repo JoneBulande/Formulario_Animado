90e1a3d641382ab849d670d643a166e084d73d0e
Jone Bulande <JoneBulande@gmail.com>
2020-08-04 12:18:02 +0200

    separei os codico

--- js/script - Copia.js
+++ js/script - Copia.js
@@ -1,12 +1,10 @@
 const form = document.querySelector("form");
 const btnLogin = document.querySelector(".btn");
-
 function remover() {
 	form.classList.remove("validate-error");
 }
 btnLogin.addEventListener("click", event =>{
-	event.preventDefault();
-	
+	event.preventDefault();	
 	const inputs = [...document.querySelectorAll("input")];
 	inputs.forEach(input =>{
 		if (input.value == "") {
@@ -17,14 +15,12 @@ btnLogin.addEventListener("click", event =>{
 			form.classList.add("form-hide");
 		}
 	});
-})
-
+});
 form.addEventListener("animationstart", event => {
 	if (event.animationName == "down") {
 		document.querySelector("body").style.overflow = "hidden";
 	}
 });
-
 form.addEventListener("animationend", event => {
 	if (event.animationName == "down") {
 		form.style.display = "none";
--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
 const form = document.querySelector("form");
-const btnLogin = document.querySelector(".btn");
 
+const btnLogin = document.querySelector(".btn");
 btnLogin.addEventListener("click", event => {
 	event.preventDefault();
-	
-	const inputs = [...document.querySelectorAll("input")];
-	inputs.forEach(input => {
-		if (input.value == "") form.classList.add("validate-error");
+	const campos = [...document.querySelectorAll("input")];
+	campos.forEach(campo =>{
+		if (campo.value == "") {
+			form.classList.add("validate-error");
+		}
 	});
-	const formErro = document.querySelector(".validate-error");
-	if (formErro) {
-		formErro.addEventListener("animationend", event => {
+	const formError = document.querySelector(".validate-error");
+	if (formError) {
+		formError.addEventListener("animationend", event =>{
 			if (event.animationName == "nono") {
 				form.classList.remove("validate-error");
 			}
-		})
+		});
 	}else{
 		form.classList.add("form-hide");
 	}
-		
-})
+});
 form.addEventListener("animationstart", event => {
 	if (event.animationName == "down") {
 		document.querySelector("body").style.overflow = "hidden";
 	}
 });
+
 form.addEventListener("animationend", event => {
 	if (event.animationName == "down") {
-		form.style.display = "none";
 		document.querySelector("body").style.overflow = "none";
 	}
 });
